03f5d62670a60e8977aae455e83cf083c3cdaaad
lua-lasalle <lua.unilasalle@gmail.com>
2021-08-18 19:50:54 -0300

    Create Tabuada.java

---
+++ Tabuada.java
@@ -0,0 +1,13 @@
+/*  Luana da Costa Bueno
+    Matricula: 0050014451    */
+    
+    public class tabuada {
+	
+		public static void main(String[] args) {
+			
+			for (int i = 0; i<=10; i++) {
+				System.out.println ("10 x " + i + " = " + (10*i));
+				}
+			}
+
+		}
